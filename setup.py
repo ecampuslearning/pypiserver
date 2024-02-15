643dcdf4f7a4475ef36eeb9fc448e2fb2921e8ad
Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>
2024-02-15 04:02:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A function that returns 'world'",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ceddlyburge/python_world",
+    url="https://github.com/ecampuslearning/python_world",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
